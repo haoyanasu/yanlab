a77c81293ae150d58b73b514294fd4bef3a048ff
Yan Lab <haoyanasu>
2025-10-30 14:08:28 -0700

    Checking if the CI is working

--- yanlab-website/src/pages/OurTeam.jsx
+++ yanlab-website/src/pages/OurTeam.jsx
@@ -180,6 +180,7 @@ const OurTeam = () => {
             <div style={{ margin: '2rem 0 0 0', textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
               <div style={{ fontWeight: 600, fontSize: '1.5rem', color: '#232946', marginBottom: '0.5rem', textAlign: 'center' }}>Graduate Students</div>
               <div style={{ marginBottom: '2rem', display: 'inline-block', textAlign: 'left' }}>
+                <b>Testing</b>
                 <b>1. Lu Yu (Graduate student, Fall 2019)</b><br />Postdoc, University of Washington<br /><br />
                 <b>2. Liangxiao Chen (Graduate student, Fall 2019)</b><br />Postdoc, Harvard University<br /><br />
                 <b>3. Leeza Abraham (Graduate student, Fall 2018)</b><br />Senior Scientist, Exodigm Biosciences<br /><br />
